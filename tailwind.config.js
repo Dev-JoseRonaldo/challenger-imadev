1ae5c25f7fdcd5dc67852bb734de3dc95a9f30c0
José Ronaldo <dev.joseronaldo@gmail.com>
2023-01-05 11:56:25 -0300

    feat: add initial variables to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
     './src/layout/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Poppins, sans-serif',
+        secondary: 'Inter, sans-serif',
+      },
+      colors: {
+        primary: '#074F97',
+        gray: {
+          500: '#4E4E4E',
+        },
+        blue: {
+          700: '#222A4A',
+        },
+      },
+      backgroundImage: {
+        testimonial:
+          'url(https://res.cloudinary.com/devjoseronaldo/image/upload/v1672868201/bg_evneu9.png)',
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 }
